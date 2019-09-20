5e40634d1584fccf78f3f94567c920879d729260
slidem <mihai.v.alexandru@gmail.com>
2019-09-20 20:52:54 +0300

    added next permutation solution

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,26 +3,47 @@ from typing import List
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        swap_idx = 0
-        for i in reversed(range(1, len(nums))):
-            if nums[i] > nums[i - 1]:
-                swap_idx = i
-                for j in range(i, len(nums)):
-                    if j == len(nums) - 1 or (nums[j] > nums[i - 1] and nums[j - 1] < nums[i - 1]):
-                        # swap
-                        nums[i - 1], nums[j] = nums[j], nums[i - 1]
-                        break
+        # first, find the descending number starting from the end
+        s = -1
+        for i in range(len(nums) - 1, 0, -1):
+            curent = nums[i]
+            next = nums[i - 1]
+            if curent > next:
+                s = i - 1
                 break
-        self.reverse(nums, swap_idx)
 
-    def reverse(self, arr, start):
-        end = len(arr)-1
-        limit = int((start + end)/2) + 1
-        for i in range(start, limit):
-            arr[i], arr[end] = arr[end], arr[i]
-            end = end - 1
+        swap_with_idx = len(nums) - 1
+        # if s = -1, that means all numbers are ordered in desc
+        if s != -1:
+            swap_with_idx = self.compute_swap_idx(nums, s)
+            # swap
+            nums[s], nums[swap_with_idx] = nums[swap_with_idx], nums[s]
+
+        # now reverse the subarray
+        self.reverse(nums, s + 1)
+
+    # search in the subarray delimited by the found idx (s) until the end of the array
+    # and find the smallest number in the subarray bigger than the number at the found idx;
+    def compute_swap_idx(self, nums, s):
+        swap_with_idx = len(nums) - 1
+
+        for i in range(s + 1, len(nums) - 1):
+            curent = nums[i]
+            next = nums[i + 1]
+            if nums[s] < curent and nums[s] >= next:
+                swap_with_idx = i
+        return swap_with_idx
+
+    # reverese in place from start (including)
+    def reverse(self, nums, start):
+        s = start
+        end = len(nums) - 1
+        while s < end:
+            nums[s], nums[end] = nums[end], nums[s]
+            s += 1
+            end -= 1
 
 
-nums = [1, 3, 2]
+nums = [3, 2, 1]
 Solution().nextPermutation(nums)
 print(nums)
